39deecfefd46185c5b1c56623c10453f3eb9ae57
Dominik Serve <serve@fs.tum.de>
2017-11-28 18:26:48 +0100

    boilerplate from exercises 2 and 3

---
+++ src/main/java/com/example/guestbook/HelloWorldResource.java
@@ -0,0 +1,18 @@
+package com.example.guestbook;
+
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+/**
+ * Resource which has only one representation.
+ *
+ */
+public class HelloWorldResource extends ServerResource {
+
+    @Get
+    public String represent() {
+        return "hello, world (from the cloud!)";
+    }
+
+}
+
